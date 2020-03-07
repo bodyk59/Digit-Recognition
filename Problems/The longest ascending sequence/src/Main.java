2dadede22d0e946a0c653477ff9d213f523b7943
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-03-07 12:57:49 +0200

    Added: new version of The longest ascending sequence, task-info.yaml to Right Rotation.

--- Problems/The longest ascending sequence/src/Main.java
+++ Problems/The longest ascending sequence/src/Main.java
@@ -4,27 +4,25 @@ import java.util.Scanner;
  * @author Bogdan Kurchak
  */
 class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int length = scanner.nextInt();
+
         int[] numberSequence = new int[length];
+
+        int count = 0;
+        int temp = numberSequence[0];
 
         for (int j = 0; j < numberSequence.length; j++) {
             numberSequence[j] = scanner.nextInt();
-        }
 
-        int count = 1;
-        int temp = 0;
-
-        for (int i = 1; i < numberSequence.length; i++) {
-            if (numberSequence[i] > numberSequence[i - 1]) {
-                temp = ++count;
-            } else {
-                count = 1;
+            if (numberSequence[j] > temp) {
+                temp = numberSequence[j];
+                count++;
             }
         }
 
-        System.out.println(numberSequence.length > 1 ? temp : 1);
+        System.out.println(count);
     }
 }
