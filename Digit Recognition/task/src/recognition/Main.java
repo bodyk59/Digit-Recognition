6f75f4579428f3824fce266eb84fa84545996aaa
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-03-07 11:08:52 +0200

    Completed following problems: 3. The longest ascending sequence

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -1,49 +1,50 @@
 package recognition;
 import java.util.*;
 
-/**
- * @author Bogdan Kurchak
- */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String line;
 
         char[][] inputValues = new char[3][3];
-
+        int[][] neuronValues = new int[3][3];
         int [][] weights = {{2, 1, 2}, {4, -4, 4}, {2, -1, 2}};
-
         int bias = -5;
         int result = 0;
 
-        result += bias;
-
-        for (int i = 0; i < inputValues.length; i++) {
+        for (int i = 0; i < neuronValues.length; i++) {
             line = scanner.next();
 
-            for (int j = 0; j < inputValues.length; j++) {
-                // Here, we set the read value to an array of character(colours)
+            for (int j = 0; j < neuronValues.length; j++) {
                 inputValues[i][j] = line.charAt(j);
 
-                if (inputValues[i][j] == 'X') {
-                    // If colour blue, the neuron value is 1 and if colour white, the neuron value is 0
-                    result +=  weights[i][j];
+                switch (inputValues[i][j]) {
+                    case '_':
+                        neuronValues[i][j] = 0;
+                        result += neuronValues[i][j] * weights[i][j];
+                        break;
+
+                    case 'X':
+                        neuronValues[i][j] = 1;
+                        result += neuronValues[i][j] * weights[i][j];
+                        break;
                 }
             }
         }
 
         System.out.println("Input grid :");
-
-        // This method print out the inputted grid
-        for (char[] lineValues : inputValues) {
-            for (char value : lineValues) {
+        for (char[] inLineValues : inputValues) {
+            for (char value : inLineValues) {
                 System.out.print(value);
             }
             System.out.println();
         }
 
-        // Here we print out the value of output neuron
-        System.out.printf("This is number is %d", result < 0 ? 1 : 0);
+        System.out.print("This is number is ");
+        if (result + bias < 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
 }
---
+++ Problems/The longest ascending sequence/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author Bogdan Kurchak
+ */
+class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] numberSequence = new int[length];
+
+        for (int j = 0; j < numberSequence.length; j++) {
+            numberSequence[j] = scanner.nextInt();
+        }
+
+        int count = 1;
+        int temp = 0;
+
+        for (int i = 1; i < numberSequence.length; i++) {
+            if (numberSequence[i] > numberSequence[i - 1]) {
+                temp = ++count;
+            } else {
+                count = 1;
+            }
+        }
+
+        System.out.println(numberSequence.length > 1 ? temp : 1);
+    }
+}
