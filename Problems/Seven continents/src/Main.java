fd1d3f64ad0d9e04db836f6e5c59a38c71f15585
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-03-11 16:25:14 +0200

    Added new version: Seven continents

--- Problems/Seven continents/src/Main.java
+++ Problems/Seven continents/src/Main.java
@@ -1,26 +1,41 @@
 import java.util.Scanner;
 
+/**
+ * @author Bogdan Kurchak
+ */
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] continentsSorted = {"Asia",
+                "Africa",
+                "North America",
+                "South America",
+                "Antarctica",
+                "Europe",
+                "Australia"};
+
         String[] continents = new String[7];
 
         for (int i = 0; i < continents.length; i++) {
             String temp = scanner.next();
 
-            if (temp.equals("North") || temp.equals("South")) {
+            if ("North".equals(temp) || "South".equals(temp)) {
                 temp += " " + scanner.next();
             }
 
             continents[i] = temp;
         }
 
-        System.out.println("Asia\n" +
-                "Africa\n" +
-                "North America\n" +
-                "South America\n" +
-                "Antarctica\n" +
-                "Europe\n" +
-                "Australia");
+        int count = 0;
+
+        while (count < 7) {
+            for (String continent : continents) {
+                if (continentsSorted[count].equals(continent)) {
+                    System.out.println(continent);
+                    count++;
+                    break;
+                }
+            }
+        }
     }
 }
