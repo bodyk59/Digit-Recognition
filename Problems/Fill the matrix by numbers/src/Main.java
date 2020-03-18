ec9f3091a00b5922595e00521aabf3ed56ec9977
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-03-18 14:23:36 +0200

    Added: new version Fill the matrix by numbers

--- Problems/Fill the matrix by numbers/src/Main.java
+++ Problems/Fill the matrix by numbers/src/Main.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
+/**
+ * @author Bogdan Kurchak
+ */
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int size = scanner.nextInt();
         int[][] matrix = new int[size][size];
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][(j + i) % matrix[i].length] = j;
-            }
-        }
+                matrix[i][j] = Math.abs(i - j);
 
-        for (int[] array : matrix) {
-            for (int number : array) {
-                System.out.print(number + " ");
+                System.out.print(matrix[i][j] + " ");
             }
+
             System.out.println();
         }
     }
