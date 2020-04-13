5dfc7a3783663e51591520b3c8147bc85262cae8
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-13 21:42:33 +0300

    Completed following problem: 55. Count words

---
+++ Problems/Count words/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        try (Reader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int character;
+            String line = "";
+
+            while ((character = reader.read()) != -1) {
+                line += (char) character;
+            }
+
+            String[] words = line.trim().split("\\s+");
+
+            System.out.println(line.isBlank() ? 0 : words.length);
+        }
+    }
+}
