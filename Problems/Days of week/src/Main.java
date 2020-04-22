9f7bfc36a032bd22d7ccbf3bec40d2c49a6ad838
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-22 21:52:36 +0300

    Completed following problem: 67. Days of week

---
+++ Problems/Days of week/src/Main.java
@@ -0,0 +1,12 @@
+// Posted from EduTools plugin
+enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+public class Main {
+    public static void main(String[] args) {
+        for (DayOfWeek day : DayOfWeek.values()) {
+            System.out.println(day);
+        }
+    }
+}
