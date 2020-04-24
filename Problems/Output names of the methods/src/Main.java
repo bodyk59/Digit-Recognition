c067de6af92027dc8cb22ff2043e2b87e6546aa5
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-24 23:14:25 +0300

    Completed following problem: 91. Output names of the methods

---
+++ Problems/Output names of the methods/src/Main.java
@@ -0,0 +1,12 @@
+SuperClass instance = new SuperClass() {
+
+    @Override
+    public void method2() {
+        System.out.println("method2");
+    }
+
+    @Override
+    public void method3() {
+        System.out.println("method3");
+    }
+};
