9b4f5198c12638194cc4f144bf8b5a160b0ad879
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-08 17:52:28 +0300

    Completed following problem: 126. Getting current thread info

---
+++ Problems/Getting current thread info/src/Main.java
@@ -0,0 +1,5 @@
+public static void printCurrentThreadInfo() {
+    Thread thread = Thread.currentThread();
+    System.out.println("name: " + thread.getName());
+    System.out.println("priority: " + thread.getPriority());
+}
