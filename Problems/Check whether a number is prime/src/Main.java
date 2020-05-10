f3d0d7b3654ac41dfef5677a75b86c9a53ccc8d8
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-10 20:04:30 +0300

    Completed following problem: 142. Check whether a number is prime

---
+++ Problems/Check whether a number is prime/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        while (scanner.hasNext()) {
+            int number = scanner.nextInt();
+            executor.submit(new PrintIfPrimeTask(number));
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+    }
+}
+
+class PrintIfPrimeTask implements Runnable {
+    private final int number;
+
+    public PrintIfPrimeTask(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        int counter = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                counter++;
+            }
+        }
+        if (counter == 2) {
+            System.out.println(number);
+        }
+    }
+}
