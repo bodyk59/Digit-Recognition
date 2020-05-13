fbafa4add4a4351f97238fe5abea89bf4312fd14
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-13 18:42:05 +0300

    Completed following problem: 159. Find the latest date-time

---
+++ Problems/Find the latest date-time/src/Main.java
@@ -0,0 +1,19 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime max = LocalDateTime.MIN;
+        int counter = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < counter; i++) {
+            LocalDateTime temp = LocalDateTime.parse(scanner.nextLine());
+            if (temp.isAfter(max)) {
+                max = temp;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
