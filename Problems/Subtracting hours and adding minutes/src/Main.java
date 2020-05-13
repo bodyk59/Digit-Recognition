c953a98c3e46e804b0f106998840684f76caea8f
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-13 18:48:53 +0300

    Completed following problem: 168. Subtracting hours and adding minutes

---
+++ Problems/Subtracting hours and adding minutes/src/Main.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime localDateTime = LocalDateTime.parse(scanner.nextLine())
+                .minusHours(scanner.nextInt())
+                .plusMinutes(scanner.nextInt());
+        System.out.println(localDateTime);
+    }
+}
