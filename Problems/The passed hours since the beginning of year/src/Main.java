c207bd7329b0953fc588f677917ebf4fbf163c39
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-13 18:54:03 +0300

    Completed following problem: 175. The passed hours since the beginning of year

---
+++ Problems/The passed hours since the beginning of year/src/Main.java
@@ -0,0 +1,12 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime currentDate = LocalDateTime.parse(scanner.nextLine());
+        LocalDateTime startOfTheYear = LocalDateTime.of(currentDate.getYear(), 1, 1, 0, 0);
+        System.out.println(Duration.between(startOfTheYear, currentDate).toHours());
+    }
+}
