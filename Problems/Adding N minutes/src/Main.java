385b80253bc6416442b18d90943343e5157ca7da
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-13 18:39:29 +0300

    Completed following problem: 155. Adding N minutes

---
+++ Problems/Adding N minutes/src/Main.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime localDateTime = LocalDateTime.parse(scanner.nextLine()).plusMinutes(scanner.nextInt());
+        System.out.printf("%d %d %s",
+                localDateTime.getYear(),
+                localDateTime.getDayOfYear(),
+                localDateTime.toLocalTime());
+    }
+}
