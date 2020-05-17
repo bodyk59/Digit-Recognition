7df2cc469c723bc8d80334fd5cd35770e3eab1d7
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-17 10:42:13 +0300

    Completed following problem: 177. Bad words detecting

---
+++ Problems/Bad words detecting/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.*;
+
+class BadWordsDetector {
+
+    private static Stream<String> createBadWordsDetectingStream(String text, 
+                                                                List<String> badWords) {
+        return badWords.stream()
+                .filter(text::contains)
+                .distinct()
+                .sorted();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] parts = scanner.nextLine().split(";");
+
+        String text = parts[0];
+
+        List<String> dict = parts.length > 1 ?
+                Arrays.asList(parts[1].split(" ")) :
+                Collections.singletonList("");
+
+        System.out.println(createBadWordsDetectingStream(text, dict).collect(Collectors.toList()));
+    }
+
+}
