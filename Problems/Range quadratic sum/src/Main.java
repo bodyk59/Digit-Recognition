5b80dc9ff101e54e5da2dd4916e1824a5ad66acd
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-17 13:18:46 +0300

    Completed following problem: 188. Range quadratic sum

---
+++ Problems/Range quadratic sum/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+
+class QuadraticSum {
+    public static long rangeQuadraticSum(int fromIncl, int toExcl) {
+        return LongStream.range(fromIncl, toExcl)
+                .map(x -> (long) Math.pow(x, 2))
+                .sum();
+    }
+}
